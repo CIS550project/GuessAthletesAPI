1f7762bf5f8126ec6354d2425eea99dd9e63662e
Colin Zhang <czhang@czlayer.com>
2016-12-13 15:45:20 -0500

    handle mysql disconnects

--- database.js
+++ database.js
@@ -2,6 +2,20 @@ var exports = module.exports = {};
 var config = require('./config');
 var mysql = require('mysql');
 var pool = mysql.createPool(config.mysql);
+handleDisconnect(pool);
+
+function handleDisconnect(client) {
+  client.on('error', function (error) {
+    if (!error.fatal) return;
+    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+
+    console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+    pool = mysql.createPool(config.mysql);
+    handleDisconnect(pool);
+    pool.connect();
+  });
+}
 
 exports.query = function(sql, values, callback) {
   pool.getConnection(function(err, connection) {
